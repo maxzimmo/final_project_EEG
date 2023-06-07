6ab52dcbe05bb9d84a289831d0034637223d775f
Julian Morales <cacao38@gmail.com>
2023-06-07 12:22:34 +0200

    organized funcs into Internal and Data_Access for the globals

--- final_project_EEG/Data_Access.py
+++ final_project_EEG/Data_Access.py
@@ -1,55 +1,21 @@
 import numpy as np
 import pandas as pd
 import pickle
+import seaborn as sns
+import random
+
+from sklearn.preprocessing import MinMaxScaler
+from final_project_EEG import Internal_funcs
 
 # versions used by source
 #print(np.__version__) #1.18.4
 #print(pickle.format_version)
 
-def splitdata(X, y, ntrainbatch=10):
-    nbatch=ntrainbatch-1
-    trainframes=[]
-    testframes =[]
-    for i in range(45):
-        if i%15-1<nbatch:
-            trainframes.append(pd.concat([pd.DataFrame(X[i]), pd.DataFrame(y[i])], axis=1))
-        if i%15>nbatch:
-            testframes.append(pd.concat([pd.DataFrame(X[i]), pd.DataFrame(y[i])], axis=1))
-    train = pd.concat(trainframes)
-    test  = pd.concat(testframes)
-    return train, test
-
-def gatherdata(X, y):
-    Xyframes=[]
-    for i in range(45):
-        Xyframes.append(pd.concat([pd.DataFrame(X[i]), pd.DataFrame(y[i])], axis=1))
-    XyDF = pd.concat(Xyframes)
-    return XyDF
-
-#Full DF no split
-def fulldf(nsubjects=16):
-    '''Files must be labelled as {subject#}_123.npz' and should be inside a folder named 'Data' within the Project.
-    To use: insantiate as follows: XyDF = fulldf()
-    '''
-    data16  = {}
-    label16 = {}
-    for i in range(1,nsubjects+1): 
-        # Load all 16 files data into a Dict named 'i_123.npz' using a for loop
-        data16[i]  = pickle.loads(np.load(f'../data/{i}_123.npz')['data'])
-        label16[i] = pickle.loads(np.load(f'../data/{i}_123.npz')['label'])
-    Xy16_list = []
-    for i in range(1,nsubjects+1): 
-        #apply all data to the gather data func to create lists of DFs 
-        Xy = gatherdata(data16[i], label16[i])
-        Xy16_list.append(Xy)
-    XyDF = pd.concat(Xy16_list)
-    return XyDF
-#dddd
+# GLOBAL FUNCS
 
 #Train-Test from Full DF function 
 def fulldfsplit(nsubjects=16):
-    '''Files must be labelled as {subject#}_123.npz' and should be inside a Data folder within the Project
-    Insantiate as follows: Xytrain16_DF, Xytest16_DF = fulldfsplit()'''
+    '''Files must be labelled as {subject#}_123.npz' and should be inside a Data folder within the Project'''
     data16  = {}
     label16 = {}
     Xytrain16_list = []
@@ -70,3 +36,47 @@ def fulldfsplit(nsubjects=16):
     Xytest16_DF  = pd.concat(Xytest16_list)
     
     return Xytrain16_DF, Xytest16_DF
+
+#Full DF no split
+def fulldf(nsubjects=16):
+    '''Files must be labelled as {subject#}_123.npz' and should be inside a Data folder within the Project.'''
+    data16  = {}
+    label16 = {}
+    for i in range(1,nsubjects+1): 
+        # Load all 16 files data into a Dict named 'i_123.npz' using a for loop
+        data16[i]  = pickle.loads(np.load(f'../data/{i}_123.npz')['data'])
+        label16[i] = pickle.loads(np.load(f'../data/{i}_123.npz')['label'])
+    Xy16_list = []
+    for i in range(1,nsubjects+1): 
+        #apply all data to the gather data func to create lists of DFs 
+        Xy = gatherdata(data16[i], label16[i])
+        Xy16_list.append(Xy)
+    XyDF = pd.concat(Xy16_list)
+    XyDF.columns = [*XyDF.columns[:-1], 'target']
+    return XyDF
+
+#Full DF, no split, slicing each clip to multiples of 13
+def fulldfslices(nsubjects=16, slice_size =13, trackdict=False):
+    '''Files must be labelled as {subject#}_123.npz' and should be inside a Data folder within the Project.
+    slice_size is the desired row length of each slice
+    trackdict returns a dict with the slices per clip. To retrieve: df, dicc = fulldfslices(trackdict=True)
+    '''
+    data16  = {}
+    label16 = {}
+    for i in range(1,nsubjects+1): 
+        # Load all 16 files data into a Dict named 'i_123.npz' using a for loop
+        data16[i]  = pickle.loads(np.load(f'../data/{i}_123.npz')['data'])
+        label16[i] = pickle.loads(np.load(f'../data/{i}_123.npz')['label'])
+    Xy16_list = []
+    dicc16  = {}
+    for i in range(1,nsubjects+1): 
+        #apply all data to the gather data func to create lists of DFs 
+        Xy, dicc = allsets(data16[i], label16[i], slice_size)
+        Xy16_list.append(Xy)
+        dicc16[f"subject {i}"]=dicc  #list with dicc {clip,slice}
+    XyDF = pd.concat(Xy16_list)
+    XyDF.columns = [*XyDF.columns[:-1], 'target']
+    if trackdict:
+        return XyDF, dicc16
+    if not trackdict:
+        return XyDF
