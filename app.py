2e71a9ba05aa667b661f07312b78d59ef27f4774
mason tan <masonev@gmail.com>
2023-06-13 15:09:36 +0200

    updated with Julian predicted plot

--- app.py
+++ app.py
@@ -14,6 +14,16 @@ def load_css(file_name):
 # Load the model once instead of every time the function is called
 model = models.load_model("./model.h5")
 
+# Define your label dictionary
+label_dict = {0: 'Disgust', 1: 'Fear', 2: 'Sad', 3: 'Neutral', 4: 'Happy'}
+
+# Define your images dictionary
+image_dict = {0: 'https://media.istockphoto.com/id/627798010/photo/man-disgusted.jpg?s=612x612&w=0&k=20&c=OL6W0mUlXyn4RJKFdpBFNdqWapb5zYJO2_jbIMuErmE=',
+              1: 'https://musimorphic.com/wp-content/uploads/2022/10/home-alone-1024x768.jpg',
+              2: 'https://media.istockphoto.com/id/151557041/photo/baby-crying.jpg?s=612x612&w=0&k=20&c=PR6N_B-8TRjeyBVPzud5Gw_sJZZlf3wOgtg_4-AmGbM=',
+              3: 'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-760w,f_auto,q_auto:best/newscms/2016_06/973451/guy-raised-eyebrow-today-160212.jpg',
+              4: 'https://1000wordphilosophy.files.wordpress.com/2021/05/happiness.jpg?w=640'}
+
 def predict(input_file):
     '''needs a file path as input. Loads .npy file and predicts. Returns prediction'''
     X_test = np.load(input_file)
@@ -67,15 +77,38 @@ def main():
             file.write(uploaded_file.getvalue())
         st.success(f"File '{uploaded_file.name}' uploaded successfully.")
 
+        # Get file summary
+        X_test = np.load(file_path)
+        st.info(
+            f"The uploaded file contains {X_test.shape[0]} sets to be predicted.")
+
         # Run prediction
         output = predict(file_path)
-        final_trial_predict = np.max(output[0])
-        st.markdown(f"**Prediction Result:** {final_trial_predict}")
-        print(output)
-        st.write(output)
-        plt.plot(output)
-        st.pyplot(plt)
+        st.success("Congratulations, now we have predictions!")
 
+        # Use a dropdown to select the observation to display
+        selected_observation = st.selectbox(
+            '## Choose an observation to display:', options=range(len(output)))
+
+        # Get prediction for selected observation
+        selected_output = output[selected_observation]
+
+        # Get the prediction category with the highest probability
+        best_category = np.argmax(selected_output)
+
+        st.markdown(
+            f"**The emotion predicted is:** {label_dict[best_category]}")
+        st.image(image_dict[best_category], use_column_width=True)
+
+        st.markdown(
+            f'''{label_dict[best_category]} is the Emotion with the highest probability.''')
+        st.markdown(f"{round(selected_output[best_category],2)}")
+        # Create a bar plot for the probabilities of each category
+        fig, ax = plt.subplots()
+        ax.bar(label_dict.values(), selected_output)
+        ax.set_xlabel('Emotion')
+        ax.set_ylabel('Probability')
+        st.pyplot(fig)
 
 if __name__ == '__main__':
     main()
