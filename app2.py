3366607bad83bcaec252621e7c409c6435ccd544
Julian Morales <cacao38@gmail.com>
2023-06-14 15:17:00 +0200

    added from backend

--- app2.py
+++ app2.py
@@ -7,17 +7,23 @@ from sklearn.metrics import confusion_matrix
 import seaborn as sns
 
 # Load the model once instead of every time the function is called
-model = models.load_model("./model.h5")
+model = models.load_model("./data/models/model.h5")
 
 # Define your label dictionary
 label_dict = {0:'Disgust', 1:'Fear', 2:'Sad', 3:'Neutral', 4:'Happy'}
 
 # Define your images dictionary
-image_dict = {0: 'https://media.istockphoto.com/id/627798010/photo/man-disgusted.jpg?s=612x612&w=0&k=20&c=OL6W0mUlXyn4RJKFdpBFNdqWapb5zYJO2_jbIMuErmE=',
-              1: 'https://musimorphic.com/wp-content/uploads/2022/10/home-alone-1024x768.jpg',
-              2: 'https://media.istockphoto.com/id/151557041/photo/baby-crying.jpg?s=612x612&w=0&k=20&c=PR6N_B-8TRjeyBVPzud5Gw_sJZZlf3wOgtg_4-AmGbM=',
-              3: 'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-760w,f_auto,q_auto:best/newscms/2016_06/973451/guy-raised-eyebrow-today-160212.jpg',
-              4: 'https://1000wordphilosophy.files.wordpress.com/2021/05/happiness.jpg?w=640'}
+#image_dict = {0: 'https://media.istockphoto.com/id/627798010/photo/man-disgusted.jpg?s=612x612&w=0&k=20&c=OL6W0mUlXyn4RJKFdpBFNdqWapb5zYJO2_jbIMuErmE=',
+#              1: 'https://musimorphic.com/wp-content/uploads/2022/10/home-alone-1024x768.jpg',
+#              2: 'https://media.istockphoto.com/id/151557041/photo/baby-crying.jpg?s=612x612&w=0&k=20&c=PR6N_B-8TRjeyBVPzud5Gw_sJZZlf3wOgtg_4-AmGbM=',
+#              3: 'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-760w,f_auto,q_auto:best/newscms/2016_06/973451/guy-raised-eyebrow-today-160212.jpg',
+#              4: 'https://1000wordphilosophy.files.wordpress.com/2021/05/happiness.jpg?w=640'}
+image_dict = {0: './data/data/emotion_images/disgust.jpeg',
+              1: './data/emotion_images/fear.jpeg',
+              2: './data/emotion_images/sad.jpeg',
+              3: './data/emotion_images/neutral.jpeg',
+              4: './data/emotion_images/happy.jpeg'}
+
 
 def predict(input_file):
     '''needs a file path as input. Loads .npy file and predicts. Returns prediction'''
@@ -33,7 +39,7 @@ def main():
     Upload a file to get a prediction!''')
 
     # Load y_test data
-    y_test = np.load('y_test.npy')
+    y_test = np.load('./data/models/y_test.npy')
 
     # Create a directory for file uploads
     upload_dir = "upload"
@@ -67,16 +73,16 @@ def main():
         # Get the prediction category with the highest probability
         best_category = np.argmax(selected_output)
         
-        st.markdown(f"**The emotion predicted is:** {label_dict[best_category]}")
         st.image(image_dict[best_category], use_column_width=True)
-        
+        st.markdown(f"## **The emotion predicted is:** {label_dict[best_category]}")
+
         predicted_acc = max(selected_output)*100 
         formatted_string = "{:.2f}".format(predicted_acc)
         # format to two decimal places
         float_value = float(formatted_string)
         
         #Summary Analysis
-        st.markdown(f'''For this set, the most likely Emotion is "{label_dict[best_category]}", given the highest probability:   {float_value} %''')
+        st.markdown(f'''For this set, the most likely Emotion is '{label_dict[best_category]}', with a probability of: {float_value} %''')
 
         # Create a bar plot for the probabilities of each category
         fig, ax = plt.subplots()
