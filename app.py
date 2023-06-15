821e0aef03903b63c9eb3fcb9471559761c2116c
Julian Morales <cacao38@gmail.com>
2023-06-15 17:48:50 +0200

    add emergency button

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def main():
     st.write("""
         <div class="container d-flex flex-column justify-content-center align-items-center vh-100">
             <h1 class="text-center p-2 display-4 mt-5 eeg-title">E.E.G</h1>
-            <p class="text-center eeg-caption">Emotion Predictor</p>
+            <p class="text-center eeg-caption">The Emotion Predictor</p>
             <div class="eeg-description">
                 <p>
                     Welcome to the emotion predictor! Upload a file to get a prediction
@@ -69,25 +69,37 @@ def main():
     # Create a directory for file uploads
     upload_dir = "upload"
     os.makedirs(upload_dir, exist_ok=True)
-
+    
+    #---------------------
+    
     # Upload file
     uploaded_file = st.file_uploader("Choose a .npy file", type=['npy'])
 
-    # Check if file was uploaded
-    if uploaded_file is not None:
-        # Save the uploaded file to the upload directory
-        file_path = os.path.join(upload_dir, uploaded_file.name)
-        with open(file_path, "wb") as file:
-            file.write(uploaded_file.getvalue())
-        st.success(f"File '{uploaded_file.name}' uploaded successfully.")
+    default_file_button = False
+    if uploaded_file is None:
+        default_file_button = st.button("*")
+
+    # Check if file was uploaded or default file button was clicked
+    if uploaded_file is not None or default_file_button:
+        if uploaded_file is not None:
+            # Save the uploaded file to the upload directory
+            file_path = os.path.join(upload_dir, uploaded_file.name)
+            with open(file_path, "wb") as file:
+                file.write(uploaded_file.getvalue())
+            st.success(f"File '{uploaded_file.name}' uploaded successfully.")
+        elif default_file_button:
+            # Use default file
+            file_path = './data/models/X_test.npy'
 
         # Get file summary
         X_test = np.load(file_path)
+    #---------------------
+
         #st.info(
         #    f"The uploaded file contains {X_test.shape[0]} sets to be predicted.")
         st.markdown(f"""
             <div style='background-color:rgba(0, 0, 255, 0.5);'>
-                <p>The uploaded file contains {X_test.shape[0]} sets to be predicted.</p>
+                <p>The uploaded file contains {X_test.shape[0]} moviesets to be predicted.</p>
             </div>
             """, unsafe_allow_html=True)
         
@@ -130,11 +142,11 @@ def main():
 
         # Create a bar plot for the probabilities of each category
         fig, ax = plt.subplots()
-        sns.set_theme(style='darkgrid', rc={'figure.figsize':(16,8)})
+        sns.set_theme(style='darkgrid', rc={'figure.figsize':(12,6)})
         sns.barplot(x=list(label_dict.values()), y=selected_output, ax=ax)
-        ax.set_title('Emotion Probability Distribution', fontsize=40)  # Increase the fontsize here
-        ax.set_xlabel('Emotion')
-        ax.set_ylabel('Probability')
+        ax.set_title('Emotion Probability Distribution', fontsize=40)
+        ax.set_xlabel('Emotions', fontsize=20)
+        ax.set_ylabel('Probability', fontsize=20)
         st.pyplot(fig)
 
 
